b1e38ee55b75dbfb9623ec4b8ee23c71ab0be7fb
sir bobz <bobcertified@gmail.com>
2019-10-26 21:33:43 +0300

    created the processing method

--- app/Http/Controllers/InitiatePaymentController.php
+++ app/Http/Controllers/InitiatePaymentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use SmoDav\iPay\Cashier;
+use Exception;
 
 class InitiatePaymentController extends Controller
 {
@@ -14,5 +16,23 @@ class InitiatePaymentController extends Controller
     public function __construct()
     {
         //
+    }
+
+    public function index(){
+    	try 
+    	{
+		   $cashier = new Cashier();
+		   return $response = $cashier
+			    ->usingVendorId('ecml', 'edgecityk58d8e8dd')
+			    ->withCallback('http://yourcallback.com')
+			    ->withCustomer('0721553678', 'demo@example.com', false)
+			    ->transact(1, 'your order id', 'your order secret');
+
+			//return view('response', compact('response'));
+
+    	} catch (Exception $e) {
+    		
+    	}
+
     }
 }
