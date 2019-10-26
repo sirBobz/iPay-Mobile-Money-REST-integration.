f2bea75b459ebe3dfa4ee1bf1f7bb6293c864b17
sir bobz <bobcertified@gmail.com>
2019-10-26 22:02:38 +0300

    registered the logger class on the app config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -57,9 +57,9 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\RequestResponseLogger::class,
+]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
